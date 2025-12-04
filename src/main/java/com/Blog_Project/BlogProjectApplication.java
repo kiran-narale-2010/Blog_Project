09b6b89e5b77c4597b7e2d3672492791d7e2c096
kiran-java <kirannarale2010@gmail.com>
2025-12-04 23:09:16 +0530

    This are the important File for Project

---
+++ src/main/java/com/Blog_Project/BlogProjectApplication.java
@@ -0,0 +1,14 @@
+package com.Blog_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogProjectApplication.class, args);
+		System.out.println("hello Springboot");
+	}
+
+}
